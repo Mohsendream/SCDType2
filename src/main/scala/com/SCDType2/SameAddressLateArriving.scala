9c8787baff3ef4daa16ea99b8a1ead45729392a8
Mohsendream <madiounimohsen@gmail.com>
2023-04-06 12:45:29 +0200

    SameAddressLateArriving done

---
+++ src/main/scala/com/SCDType2/SameAddressLateArriving.scala
@@ -0,0 +1,23 @@
+package com.SCDType2
+
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions._
+
+object SameAddressLateArriving {
+
+  def sameAddressLateArriving(historyDataframe: DataFrame, updatesDataframe: DataFrame, spark: SparkSession): DataFrame = {
+    val lateArrivingSameIdAddressDataframe = historyDataframe.join(updatesDataframe, historyDataframe.col("Id") === updatesDataframe.col("newId"), "inner")
+      .where(col("newAddress") === col("Address"))
+      .where(col("newMovedIn") <= col("movedIn"))
+    val updatedHistory_1 = lateArrivingSameIdAddressDataframe.select(col("newId").as("Id"),
+      col("newFirstName").as("firstName"), col("newLastName").as("lastName"),
+      col("newAddress").as("address"), col("newMovedIn").as("movedIn"),
+      col("movedOut"), col("status"))
+      .withColumn("movedOut", col("movedOut"))
+      .withColumn("status", col("status"))
+      .withColumn("movedIn", col("movedIn"))
+    val bareHistory=historyDataframe.join(updatedHistory_1,historyDataframe.col("id")===updatedHistory_1.col("Id"), "left_anti")
+    val result=bareHistory.union(updatedHistory_1)
+    result
+  }
+}
---
+++ src/test/scala/com/SCDType2/SameAddressLateArrivingSpec.scala
@@ -0,0 +1,30 @@
+package com.SCDType2
+
+import com.SCDType2.SameAddressLateArriving.sameAddressLateArriving
+import org.apache.spark.sql.SparkSession
+import org.scalatest.GivenWhenThen
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class SameAddressLateArrivingSpec extends AnyFlatSpec with Matchers with GivenWhenThen {
+  implicit val spark = SparkSession
+    .builder()
+    .master("local[*]")
+    .appName("test")
+    .getOrCreate()
+
+  import spark.implicits._
+
+  "SameAddressLateArrivingSpec" should "return a history table updates with the first date when the same address" in {
+    Given("the history and the updates dataframes")
+    val updatesDataframe = Seq(Updates(1L, "Madiouni", "Mohsen", "Kef", "15-09-2009")).toDF()
+    val historyDataframe = Seq(History(1L, "Madiouni", "Mohsen", "Kef", "15-09-2010", "Null", true),
+      History(2L, "Madiouni", "Haifa", "Liege", "15-09-2020", "Null", true)).toDF()
+    When("duplicates is invoked")
+    val result = sameAddressLateArriving(historyDataframe, updatesDataframe, spark)
+    Then("the result should be returned")
+    val expectedResult = Seq(History(1L, "Madiouni", "Mohsen", "Kef", "15-09-2009", "Null", true),
+      History(2L, "Madiouni", "Haifa", "Liege", "15-09-2020", "Null", true)).toDF()
+    expectedResult.collect() should contain theSameElementsAs result.collect()
+  }
+}
