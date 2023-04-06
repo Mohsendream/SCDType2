583c9079eb566cbca4e55364026172e9b7372632
Mohsendream <madiounimohsen@gmail.com>
2023-04-06 13:37:40 +0200

    SameAddressBetweenDates done

--- src/main/scala/com/SCDType2/SameAddressLateArriving.scala
+++ src/main/scala/com/SCDType2/SameAddressLateArriving.scala
@@ -8,7 +8,7 @@ object SameAddressLateArriving {
   def sameAddressLateArriving(historyDataframe: DataFrame, updatesDataframe: DataFrame, spark: SparkSession): DataFrame = {
     val lateArrivingSameIdAddressDataframe = historyDataframe.join(updatesDataframe, historyDataframe.col("Id") === updatesDataframe.col("newId"), "inner")
       .where(col("newAddress") === col("Address"))
-      .where(col("newMovedIn") <= col("movedIn"))
+      .where(to_date(col("newMovedIn"), "dd-MM-yyyy") <= to_date(col("movedIn"),"dd-MM-yyyy"))
     val updatedHistory_1 = lateArrivingSameIdAddressDataframe.select(col("newId").as("Id"),
       col("newFirstName").as("firstName"), col("newLastName").as("lastName"),
       col("newAddress").as("address"), col("newMovedIn").as("movedIn"),
