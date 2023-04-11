b14c0b1ea33b0a1efce51e956ab1f316dc5735fc
Mohsendream <madiounimohsen@gmail.com>
2023-04-11 06:46:39 +0200

    all the cases are successfully tested

--- src/main/scala/com/SCDType2/AddressHistoryBuilder.scala
+++ src/main/scala/com/SCDType2/AddressHistoryBuilder.scala
@@ -7,17 +7,23 @@ import com.SCDType2.DifferentId.differentId
 import com.SCDType2.AfterMovedOut.afterMovedOut
 import com.SCDType2.BetweenMovingDates.betweenMovingDates
 import com.SCDType2.BeforeMovedIn.beforeMovedIn
+import com.SCDType2.SameAddressBetweenDates.sameAddressBetweenDates
+import com.SCDType2.SameAddressLateArriving.sameAddressLateArriving
+import com.SCDType2.SameAddressAfterDates.sameAddressAfterDates
 
 object AddressHistoryBuilder {
 
   def addressHistoryBuilder(historyDataframe: DataFrame, updatesDataframe: DataFrame, spark: SparkSession): DataFrame = {
 
     val unduplicateUpdates = duplicates(updatesDataframe, spark)
+    val newUpdates = sameAddressBetweenDates(historyDataframe, unduplicateUpdates, spark)
     val newHistory = differentId(historyDataframe, unduplicateUpdates, spark)
-    val joinedDataFrame = newHistory.join(unduplicateUpdates, newHistory.col("Id") === unduplicateUpdates.col("newId"), "inner")
-    val afterMovedOutHistory = afterMovedOut(joinedDataFrame,newHistory, spark)
+    val joinedDataFrame = newHistory.join(newUpdates, newHistory.col("Id") === newUpdates.col("newId"), "inner")
+    val afterMovedOutHistory = afterMovedOut(joinedDataFrame, newHistory, spark)
     val betweenMovingDatesHistory = betweenMovingDates(joinedDataFrame, afterMovedOutHistory, spark)
-    val result = beforeMovedIn(joinedDataFrame, betweenMovingDatesHistory, spark)
+    val beforeMovingDatesHistory = beforeMovedIn(joinedDataFrame, betweenMovingDatesHistory, spark)
+    val sameAddressLateArrivingHistory = sameAddressLateArriving(joinedDataFrame, beforeMovingDatesHistory, spark)
+    val result = sameAddressAfterDates(joinedDataFrame, sameAddressLateArrivingHistory, spark)
     result
   }
 }
